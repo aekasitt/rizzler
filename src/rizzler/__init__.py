ba07d9b9a46d8d489e7f92d71d9ef9feed972916
Sitt Guruvanich <aekazitt@gmail.com>
2024-06-05 02:21:49 +0700

    Match version with what's already published

--- src/rizzler/__init__.py
+++ src/rizzler/__init__.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 # Copyright (C) 2024, All rights reserved.
 # FILENAME:    ~~/src/rizzler/__init__.py
-# VERSION:     0.0.1
+# VERSION:     0.1.0
 # CREATED:     2024-06-05 01:43
 # AUTHOR: 	   Sitt Guruvanich <aekazitt+github@gmail.com>
 # DESCRIPTION:
@@ -11,7 +11,7 @@
 # *************************************************************
 """ASGI extension that provides Single-Page Application Frameworks built into templates"""
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 from .core import Rizzler
 
