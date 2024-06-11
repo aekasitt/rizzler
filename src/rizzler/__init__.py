8932a41c71ac15048a80f88757d1c946dc4d34b3
Sitt Guruvanich <aekazitt@gmail.com>
2024-06-11 15:51:08 +0700

    Version bump; 0.1.4 -> 0.1.5

--- src/rizzler/__init__.py
+++ src/rizzler/__init__.py
@@ -2,16 +2,16 @@
 # coding:utf-8
 # Copyright (C) 2024, All rights reserved.
 # FILENAME:    ~~/src/rizzler/__init__.py
-# VERSION:     0.1.4
+# VERSION:     0.1.5
 # CREATED:     2024-06-05 01:43
-# AUTHOR: 	   Sitt Guruvanich <aekazitt+github@gmail.com>
+# AUTHOR:      Sitt Guruvanich <aekazitt+github@gmail.com>
 # DESCRIPTION: https://www.w3docs.com/snippets/python/what-is-init-py-for.html
 #
 # HISTORY:
 # *************************************************************
 """ASGI extension that provides Single-Page Application Frameworks built into templates"""
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 ### Third-party packages ###
 from click import group
