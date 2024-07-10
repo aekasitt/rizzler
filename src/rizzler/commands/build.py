07ae087620c8bf688acb538de89ccd4e0e9a4f34
Sitt Guruvanich <aekazitt@gmail.com>
2024-07-10 13:28:07 +0700

    Post Vite build, create tailored template

--- src/rizzler/commands/build.py
+++ src/rizzler/commands/build.py
@@ -13,11 +13,14 @@
 ### Standard packages ###
 from asyncio import run
 from logging import Formatter, Logger, getLogger
+from re import match
 from typing import Dict, List, Tuple
 
 ### Third-party packages ###
 from click import command, option
 from rich.logging import RichHandler
+from tree_sitter import Language, Parser
+from tree_sitter_html import language
 
 ### Local modules ###
 from rizzler.core import Rizzler
@@ -65,6 +68,42 @@ def build(bun: bool, deno: bool, npm: bool, pnpm: bool, yarn: bool) -> None:
   handler.setFormatter(Formatter("%(message)s", datefmt="[%X]"))
   logger.addHandler(handler)
   run(Rizzler.build())
+
+  ### Tailor dist/index.html to be entry point ###
+  html: Language = Language(language())
+  parser: Parser = Parser(html)
+  content: bytes
+  overwrite: bytes = b""
+  with open("templates/index.html", "rb") as file:
+    content = file.read()
+  tree = parser.parse(content, encoding="utf8")
+  root_node = tree.root_node
+  doctype_node = next(filter(lambda node: node.type == "doctype", root_node.children))
+  overwrite += content[doctype_node.start_byte : doctype_node.end_byte]
+  html_node = next(filter(lambda node: node.type == "element", root_node.children))
+  cursor = html_node.walk()
+  cursor.goto_first_child()
+  while cursor.goto_next_sibling() and cursor.node is not None:
+    if (
+      match(r"^\<head\>", content[cursor.node.start_byte : cursor.node.end_byte].decode("utf8"))
+      is not None
+    ):
+      head_tag: bytes = content[cursor.node.start_byte : cursor.node.end_byte]
+      end_index: int = head_tag.decode("utf8").rindex(r"</head>")
+      overwrite += content[cursor.node.start_byte : cursor.node.start_byte + end_index]
+      overwrite += b'<link href="./rizz.css" rel="stylesheet">\n'
+      overwrite += content[cursor.node.start_byte + end_index : cursor.node.end_byte]
+    elif (
+      match(r"^\<body\>", content[cursor.node.start_byte : cursor.node.end_byte].decode("utf8"))
+      is not None
+    ):
+      body_tag: bytes = content[cursor.node.start_byte : cursor.node.end_byte]
+      end_index: int = body_tag.decode("utf8").rindex(r"</body>")
+      overwrite += content[cursor.node.start_byte : cursor.node.start_byte + end_index]
+      overwrite += b'<script language="javascript" src="./rizz.js" type="module"></script>'
+      overwrite += content[cursor.node.start_byte + end_index : cursor.node.end_byte]
+  with open("dist/index.html", "wb") as file:
+    file.write(overwrite)
 
 
 __all__ = ("build",)
