f25c833034354d4768b0e4a42df71fc28bc695e3
Sitt Guruvanich <aekazitt@gmail.com>
2024-07-10 16:38:43 +0700

    Version bump; 0.1.8 -> 0.1.9

--- src/rizzler/templating/__init__.py
+++ src/rizzler/templating/__init__.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 # Copyright (C) 2024, All rights reserved.
 # FILENAME:    ~~/src/rizzler/templating/__init__.py
-# VERSION:     0.1.8
+# VERSION:     0.1.9
 # CREATED:     2024-06-07 01:39
 # AUTHOR:      Sitt Guruvanich <aekazitt+github@gmail.com>
 # DESCRIPTION: https://www.w3docs.com/snippets/python/what-is-init-py-for.html
